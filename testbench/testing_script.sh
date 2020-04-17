e5859333e98047477d8e53adc6622e6ff85d83b7
Julian Toledano <jtoledanodiaz@gmail.com>
2020-04-17 11:47:16 +0200

    testeing_script parameters added
     * UPDATE: testing_script time and threads are now args parameters.

--- testbench/testing_script.sh
+++ testbench/testing_script.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+TEMP=`getopt -o t:c: --long time:,concurrent: -- "$@"`
+eval set -- "$TEMP"
 
-ab -t 120 -c 10 http://localhost:8090/hello > native.txt &
-ab -t 120 -c 10 http://localhost:8091/hello > jvm.txt &
-ab -t 120 -c 10 http://localhost:8092/rest-hello-0.0.1/resources/hello > wildfly.txt &
+while true ; do
+    case "$1" in
+        -t|--time)
+            time=$2 ; shift 2 ;;
+        -c|--concurrent)
+            concurrent=$2 ; shift 2 ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+echo "Starting 3 tests with $concurrent threads during $time seconds..."
+echo "Starting native test..."
+ab -t $time -c $concurrent http://localhost:8090/hello > native.txt &
+echo "Starting jvm test..."
+ab -t $time -c $concurrent http://localhost:8091/hello > jvm.txt &
+echo "Starting wildfly test..."
+ab -t $time -c $concurrent http://localhost:8092/rest-hello-0.0.1/resources/hello > wildfly.txt &
